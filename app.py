6219f62fddc499a049cb67adcd6e4c201274be33
sudhakarkandikattu <32635960+sudhakarkandikattu@users.noreply.github.com>
2018-09-30 23:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
-import pickle
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
